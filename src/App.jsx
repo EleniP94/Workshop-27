5aa1821c7649124a6c92b326d7802ba8417ba7d8
Panorea Panagiosoulis <panoreapanagiosoulis@panoreas-air.myfiosgateway.com>
2024-02-26 17:59:32 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // Import the React library, which allows us to define components
 import React from "react";
+import Players from "./features/players/Players";
 
 
 
 // Import the Players component, which we'll use to show a list of players
+
 
 
 // Define the App component
@@ -11,9 +13,12 @@ function App() {
   // This component renders the Players component inside a div
   // This div has a class of 'App', which we could use for styling
   return (
+    <>
+   <Players/>
+    </>
     
   );
 }
 
 // Export the App component as the default export
-
+export default App();
